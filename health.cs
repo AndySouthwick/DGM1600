1ab53081d55f31cda590ffa81b16150d05480802
andySouthwick <asouthwick01@gmail.com>
2016-11-05 19:10:19 -0600

    health script

---
+++ health.cs
@@ -0,0 +1,11 @@
+public class myHealth{
+    public bool[] Lights;
+
+    public void Lights(){
+        if(health >=0){
+            foreach(bool i in lights){
+                lights = false;
+            }
+        }
+    }
+}
